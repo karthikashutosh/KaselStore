2294039e9bd5abed7d817947cdef3b4d7331ad19
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-09-02 13:13:46 +0530

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from "./HomePage";
+import Cart from "./CartPage";
+import About from "./AboutPage";
+import Error from "./ErrorPage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import Checkout from "./CheckoutPage";
+import PrivateRoute from "./PrivateRoute";
+
+export {
+  Home,
+  About,
+  Error,
+  Products,
+  SingleProduct,
+  Checkout,
+  PrivateRoute,
+  Cart,
+};
