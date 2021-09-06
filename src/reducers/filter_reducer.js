40c464ec3c132c0ff3a25a58e524571e25829d5c
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-09-06 22:31:44 +0530

    filter contextadded

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -7,11 +7,16 @@ import {
   UPDATE_FILTERS,
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
-} from '../actions'
+} from "../actions";
 
 const filter_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
 
-export default filter_reducer
+  if(action.type === LOAD_PRODUCTS){
+    return{...state,all_products:[...action.payload],
+    filtered_products:[...action.payload]}
+  }
+  return state;
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default filter_reducer;
