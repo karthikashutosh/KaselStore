4d58e16a2b5d58b541c29c8846c2d694b10d8512
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-09-06 19:22:45 +0530

    cart amount functionality added

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -8,6 +8,32 @@ import AmountButtons from "./AmountButtons";
 const AddToCart = ({ product }) => {
   const { id, colors, stock } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
+
+  const [amount, setAmount] = useState(1);
+
+  const increase = () => {
+    setAmount((oldAmount)=>{
+
+      let tempAmount = oldAmount +1;
+      if(tempAmount > stock){
+        tempAmount = stock;
+      }
+return tempAmount;
+    })
+    
+  };
+  const decrease = () => {
+
+    setAmount((oldAmount)=>{
+      let tempAmount = oldAmount -1;
+
+      if(tempAmount < 1) {
+        tempAmount = 1
+      }
+      return tempAmount
+    })
+   
+  };
   return (
     <Wrapper>
       <div className="colors">
@@ -23,14 +49,22 @@ const AddToCart = ({ product }) => {
                   mainColor === color ? "color-btn active" : "color-btn"
                 }`}
               >
-                {mainColor === color ? <FaCheck /> :null}
-                
+                {mainColor === color ? <FaCheck /> : null}
               </button>
             );
           })}
         </div>
       </div>
-      <div className="btn-container"></div>
+      <div className="btn-container">
+        <AmountButtons
+          amount={amount}
+          increase={increase}
+          decrease={decrease}
+        />
+        <Link to="/cart" className="btn">
+          Add to Cart
+        </Link>
+      </div>
     </Wrapper>
   );
 };
