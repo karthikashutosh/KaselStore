febc4f02199e762626f1a6fe6767dd869954c503
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-09-09 12:19:34 +0530

    addtocart functionadded

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,33 +6,29 @@ import { useCartContext } from "../context/cart_context";
 import AmountButtons from "./AmountButtons";
 
 const AddToCart = ({ product }) => {
+  const { addToCart } = useCartContext();
   const { id, colors, stock } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
 
   const [amount, setAmount] = useState(1);
 
   const increase = () => {
-    setAmount((oldAmount)=>{
-
-      let tempAmount = oldAmount +1;
-      if(tempAmount > stock){
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1;
+      if (tempAmount > stock) {
         tempAmount = stock;
       }
-return tempAmount;
-    })
-    
+      return tempAmount;
+    });
   };
   const decrease = () => {
-
-    setAmount((oldAmount)=>{
-      let tempAmount = oldAmount -1;
-
-      if(tempAmount < 1) {
-        tempAmount = 1
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1;
+      if (tempAmount < 1) {
+        tempAmount = 1;
       }
-      return tempAmount
-    })
-   
+      return tempAmount;
+    });
   };
   return (
     <Wrapper>
@@ -61,7 +57,11 @@ return tempAmount;
           increase={increase}
           decrease={decrease}
         />
-        <Link to="/cart" className="btn">
+        <Link
+          to="/cart"
+          className="btn"
+          onClick={() => addToCart(id, mainColor, amount, product)}
+        >
           Add to Cart
         </Link>
       </div>
--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -4,11 +4,45 @@ import {
   COUNT_CART_TOTALS,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
-} from '../actions'
+} from "../actions";
 
 const cart_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  if (action.type === ADD_TO_CART) {
+    const { id, color, amount, product } = action.payload;
 
-export default cart_reducer
+    const tempItem = state.cart.find((i) => i.id === id + color);
+    if (tempItem) {
+      const tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === id + color) {
+          let newAmount = cartItem.amount + amount;
+
+          if (newAmount > cartItem.max) {
+            newAmount = cartItem.max;
+          }
+          return {
+            ...cartItem,
+            amount: newAmount,
+          };
+        } else {
+          return cartItem;
+        }
+      });
+      return { ...state, cart: tempCart };
+    } else {
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        price: product.price,
+        amount,
+        image: product.images[0].url,
+        color,
+        max: product.stock,
+      };
+      return { ...state, cart: [...state.cart, newItem] };
+    }
+  }
+
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default cart_reducer;
