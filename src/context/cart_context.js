d52501ef0fc0bcc4fdab7a8daf6c820ff34d0aa2
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-09-10 10:24:57 +0530

    cart page added

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,8 +8,17 @@ import {
   COUNT_CART_TOTALS,
 } from "../actions";
 
+const getLocalStorage = () => {
+  let carts = localStorage.getItem("cart");
+  if (carts) {
+    return JSON.parse(localStorage.getItem("cart"));
+  } else {
+    return [];
+  }
+};
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
   shipping_fee: 500,
@@ -33,6 +42,10 @@ export const CartProvider = ({ children }) => {
   //clear cart
 
   const clearCart = () => {};
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
   return (
     <CartContext.Provider
       value={{ ...state, addToCart, toggleAmount, removeCart, clearCart }}
