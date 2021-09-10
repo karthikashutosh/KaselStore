45cccd6649c2e6458d2e8836506c483191bb8df1
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-09-10 11:33:03 +0530

    CART toal added

--- src/components/CartColumns.js
+++ src/components/CartColumns.js
@@ -1,9 +1,20 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const CartColumns = () => {
-  return <h4>cart columns</h4>
-}
+  return (
+    <Wrapper>
+      <div className="content">
+        <h5>Item</h5>
+        <h5>Price</h5>
+        <h5>Quantitiy</h5>
+        <h5>Amount</h5>
+      </div>
+      <span></span>
+      <hr />
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   display: none;
@@ -29,6 +40,6 @@ const Wrapper = styled.div`
       margin-bottom: 3rem;
     }
   }
-`
+`;
 
-export default CartColumns
+export default CartColumns;
