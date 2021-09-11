8c7c831511684aa2c3f79db974fea08f7a9d4eb3
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-09-11 12:28:01 +0530

    checkout page added

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Products from "./ProductsPage";
 import SingleProduct from "./SingleProductPage";
 import Checkout from "./CheckoutPage";
 import PrivateRoute from "./PrivateRoute";
+import AuthWrapper from "./AuthWrapper";
 
 export {
   Home,
@@ -16,4 +17,5 @@ export {
   Checkout,
   PrivateRoute,
   Cart,
+  AuthWrapper,
 };
