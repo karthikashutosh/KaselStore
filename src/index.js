55bd9e1e5a88a6755313c70090ea1041559b8210
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-09-13 17:40:38 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -8,14 +8,12 @@ import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-//domain ID -- dev-as13lucr.us.auth0.com
 
-//cient ID --  u07eHx565n5HzE9Oaqle2CJxHq5I0jcx
 
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-as13lucr.us.auth0.com"
-    clientId="u07eHx565n5HzE9Oaqle2CJxHq5I0jcx"
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation="localstorage"
   >
